867e73fe6e5e6d05c48bcccb1a7dd74c6c56a2a9
Gregor Boehl <admin@gregorboehl.com>
2021-05-25 10:58:02 +0200

    resolve jl-numba problems

--- hank_bbl_tools/__init__.py
+++ hank_bbl_tools/__init__.py
@@ -21,7 +21,7 @@ def setfield(obj, field, val):
     return japi.tmp_obj
 
 
-def store_hank(path, hank, stst, A, B, gx=None, hx=None):
+def hank2dict(hank, stst, A, B, gx=None, hx=None, path=None):
 
     var_list = []
     for i in range(len(stst.indexes.distr_m)):
@@ -84,4 +84,42 @@ def store_hank(path, hank, stst, A, B, gx=None, hx=None):
 
     rdict['vars'] = var_list
 
-    return np.savez_compressed(path, **rdict)
+    if path is not None: 
+        np.savez_compressed(path, **rdict)
+
+    return rdict
+
+
+def mat2dict(path, shock_states):
+
+    # load stuff
+    AA = np.loadtxt(os.path.join(path, 'AA.txt'), delimiter=',')
+    BB = np.loadtxt(os.path.join(path, 'BB.txt'), delimiter=',')
+    CC = np.loadtxt(os.path.join(path, 'CC.txt'), delimiter=',')
+    vv = np.loadtxt(os.path.join(path, 'list_of_vars.txt'), delimiter=',', dtype=str)
+
+    vv = np.array([v[2:-1] for v in vv])
+
+    # lets stick with UTF-8
+    if 'π' in vv:
+        vv[list(vv).index('π')] = 'Pi'
+    if 'πw' in vv:
+        vv[list(vv).index('πw')] = 'Piw'
+
+    # DD is the mapping from shocks to states
+    shocks = ['e_' + s for s in shock_states]
+
+    DD = np.zeros((len(vv), len(shocks)))
+    for i, v in enumerate(shock_states):
+        DD[:, shock_states.index(v)] = CC[:, list(vv).index(v)]
+
+    rdict = {}
+    rdict['AA'] = AA
+    rdict['BB'] = BB
+    rdict['CC'] = CC
+    rdict['DD'] = DD
+    rdict['vars'] = vv
+    rdict['shock_states'] = shock_states
+    rdict['shocks'] = shocks
+    
+    return rdict
